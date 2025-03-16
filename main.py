428040c8d2ec36f3c5f688ed650bf6fb6506d9c2
Icosahunter <anzelknight@gmail.com>
2025-03-16 14:32:35 -0500

    Change to using a more fuction-oriented syntax

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import xml.etree.ElementTree as ET
 from pathlib import Path
 from copy import deepcopy
+from itertools import chain
+import re
 
 class Icon:
     def __init__(self, src_filepath):
         self.src = src_filepath
         self.tree = ET.parse(self.src)
         self.root = self.tree.getroot()
-        self.content = self.tree.findall('/{*}g')[0]
+        self.content = self.tree.findall('./{*}g')[0]
         viewbox = self.root.attrib['viewBox'].split()
         self.size = float(viewbox[2]), float(viewbox[3])
 
@@ -15,19 +17,19 @@ class Icon:
         self.tree.write(filepath)
 
     def color(self, palette):
-        for n in self.tree.findall('//*[@style]'):
+        for n in self.tree.findall('.//*[@style]'):
             for k, v in palette.items():
                 n.attrib['style'] = n.attrib['style'].replace('stroke:'+k, 'stroke:'+v)
                 n.attrib['style'] = n.attrib['style'].replace('fill:'+k, 'fill:'+v)
 
-    def insert(self, icon, id):
+    def insert(self, id, icon):
 
-        elem = self.tree.findall(f"//*[@id='__{id}__']")
+        elem = self.tree.findall(f".//*[@id='__{id}__']")
 
         if len(elem):
 
             elem = elem[0]
-            elem_parent = self.tree.findall(f"//*[@id='__{id}__']..")[0]
+            elem_parent = self.tree.findall(f".//*[@id='__{id}__']..")[0]
 
             if icon:
                 x = float(elem.attrib['x'])
@@ -41,11 +43,22 @@ class Icon:
                 y3 = y + (h/2) - (h3/2)
 
                 insert_node = deepcopy(icon.content)
-                insert_node.attrib['transform'] = f'translate({x3} {y3}) scale({scale})'
+                if 'transform' in insert_node.attrib:
+                    insert_node.attrib['transform'] += f' translate({x3} {y3}) scale({scale})'
+                else:
+                    insert_node.attrib['transform'] = f' translate({x3} {y3}) scale({scale})'
 
                 elem_parent.append(insert_node)
 
             elem_parent.remove(elem)
+
+    def rotate(self, deg):
+        x = self.size[0] / 2
+        y = self.size[1] / 2
+        if 'transform' in self.content.attrib:
+            self.content.attrib['transform'] += f' rotate({deg} {x} {y})'
+        else:
+            self.content.attrib['transform'] = f' rotate({deg} {x} {y})'
 
     @staticmethod
     def _fit_size(size, desired_size):
@@ -66,6 +79,27 @@ class Environment:
         self.source = Path('./')
         self.output = Path('./output')
         self.config = Path(file)
+        self.icons = {}
+        self.formats = ['.svg']
+        self.load_icons()
+
+    def load_icons(self):
+        self.icons.clear()
+        images = chain.from_iterable([self.source.glob(f'**/*{x}') for x in self.formats])
+        for file in images:
+            self.icons[file.stem] = Icon(file)
+        print(self.icons)
+
+    def do_inst(self, icon, inst):
+        inst, args = inst.split('(')
+        args = args[0:-1].split()
+        print(f'inst: {inst}     args: {args}')
+        if inst == 'insert':
+            icon.insert(args[0], self.icons[args[1]])
+        elif inst == 'color':
+            icon.color(self.palettes[args[0]])
+        elif inst == 'rotate':
+            icon.rotate(args[0])
 
     def do_palette(self, palette_str):
         colors = palette_str.split()[1:]
@@ -73,44 +107,35 @@ class Environment:
         colors = dict(zip(colors[::2], colors[1::2]))
         self.palettes[name] = colors
 
-    def do_template(self, template_str):
-        self.template = template_str.split()[1:]
-
     def do_source(self, source_str):
         self.source = Path(source_str.split()[1])
+        self.load_icons()
 
     def do_output(self, output_str):
         self.output = Path(output_str.split()[1])
 
     def do_icon(self, icon_str):
-        icon_zip = list(zip(self.template, icon_str.split()[1:]))
-        base = [x[1] for x in icon_zip if x[0]=='base'][0]
-        name = [x[1] for x in icon_zip if x[0]=='name'][0]
-        icon = Icon((self.source / base).with_suffix('.svg'))
-        for k, v in icon_zip:
-            if k == 'palette':
-                icon.color(self.palettes[v])
-            elif k not in ['base', 'name']:
-                if v == '.':
-                    icon.insert(None, k)
-                else:
-                    icon.insert(Icon((self.source / v).with_suffix('.svg')), k)
+        icon_def = re.findall(r"[\w\-\.]+(?:\([\w\-\.\t ]+\))?", icon_str)
+        name = icon_def[0]
+        base = icon_def[1]
+        icon = deepcopy(self.icons[base])
+        for inst in icon_def[2:]:
+            self.do_inst(icon, inst)
         dest = (self.output / name).with_suffix('.svg')
         dest.parent.mkdir(exist_ok=True, parents=True)
         icon.write(dest)
+        self.icons[name] = icon
 
     def run(self):
         with open(self.config) as f:
             for line in f:
-                if line.startswith('source'):
+                if line.startswith('$source'):
                     self.do_source(line)
-                elif line.startswith('output'):
+                elif line.startswith('$output'):
                     self.do_output(line)
-                elif line.startswith('template'):
-                    self.do_template(line)
-                elif line.startswith('palette'):
+                elif line.startswith('$palette'):
                     self.do_palette(line)
-                elif line.startswith('icon'):
+                else:
                     self.do_icon(line)
 
 env = Environment('icons.txt')
